2e593ad9b1750a39b296675be116911f1e61b4db
Liana1408 <lianachagunda83@gmail.com>
2022-10-19 11:42:08 +0200

    addition of the components preferably home and navbar and assests

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="hero" >
+            <div class="card text-bg-dark text-white border-0">
+                <img src="/assets/bg.jpg" class="card-img" alt="background"height="570px" />
+                <div class="card-img-overlay d-flex flex-column justify-content-center ">
+                <div className="container">
+                <h5 class="card-title display-3 fw-bolder mb-0">NEW TRENDY ARRIVALS</h5>
+                    <p class="card-text fs-2"> 
+                    CHECK OUT ALL THE FASHION.</p>
+                   
+                </div>
+                  
+                </div>
+            </div>
+
+        </div>
+    );
+}
+export default Home;
